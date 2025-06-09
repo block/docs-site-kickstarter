4c014112cebbd3cb142aa737f999c49a57a22b38
Michael Polce <mpolce@squareup.com>
2025-06-09 10:42:45 -0700

    link updates

--- docs-infra-prototype/astro.config.ts
+++ docs-infra-prototype/astro.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     : "http://localhost:4321", */
   
   site: 'https://block.github.io',
-  base: '/docs', 
+  base: '/docs-site-kickstarter', 
   
   integrations: [
     mdx({
