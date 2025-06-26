9b13059845a1ba0d519699896fb62edeb97f5019
Andrew Lee Rubinger <alr@alrubinger.com>
2025-06-26 01:12:25 -0700

    First cut at blog impl; content collections working

---
+++ docs-infra-prototype/src/content/config.ts
@@ -0,0 +1,27 @@
+import { z, defineCollection } from 'astro:content';
+import { glob, file } from 'astro/loaders';
+
+export const collections = {
+  // Our Blog Implementation is an Astro Content Collection
+  blog: defineCollection({
+    loader: glob({ pattern: "**/*.mdx", base: "./src/content/blog/posts" }),
+    schema: z.object({
+      title: z.string(),
+      author: z.string(),
+      pubDate: z.coerce.date(),
+      snippet: z.string().optional(),
+      heroImage: z.string().optional(),
+      tags: z.array(z.string()).optional(),
+    })}),
+  // Blog Authors are also an Astro Content Collection
+  authors: defineCollection({
+    loader: file("./src/content/blog/authors.json"),
+    schema: z.object({
+      name: z.string(),
+      title: z.string(),
+      'social-github': z.string().optional(),
+      'social-linkedin': z.string().optional(),
+      'social-x': z.string().optional(),
+      avatar: z.string(),
+    })}),
+};
