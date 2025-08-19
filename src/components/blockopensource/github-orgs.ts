eb7d10dde1245ce9244d5d9891310b4ad9197a38
Andrew Lee Rubinger <alr@alrubinger.com>
2025-08-19 14:59:43 -0700

    Issue #60: Add 'Explore Our Projects' component

---
+++ src/components/blockopensource/github-orgs.ts
@@ -0,0 +1,54 @@
+import type { GitHubOrgs, GitHubOrg} from "./ExploreProjects.astro";
+
+export const githubOrganizations: GitHubOrg[] = [
+  {
+    name: "Block",
+    href: "https://github.com/block",
+    iconName: "block-open-source/block",
+  },
+  {
+    name: "Square",
+    href: "https://github.com/square",
+    iconName: "block-open-source/square",
+  },
+  {
+    name: "Square Developers",
+    href: "https://github.com/Square-Developers",
+    iconName: "block-open-source/square",
+  },
+  {
+    name: "Cash App",
+    href: "https://github.com/cashapp",
+    iconName: "block-open-source/cashapp",
+  },
+  {
+    name: "Weebly",
+    href: "https://github.com/weebly",
+    iconName: "block-open-source/weebly",
+  },
+  {
+    name: "Afterpay",
+    href: "https://github.com/afterpay",
+    iconName: "block-open-source/afterpay",
+  },
+  {
+    name: "TIDAL",
+    href: "https://github.com/tidal-music",
+    iconName: "block-open-source/tidal",
+  },
+  {
+    name: "Proto",
+    href: "https://github.com/proto-at-block",
+    iconName: "block-open-source/proto",
+  },
+  {
+    name: "c equals",
+    href: "https://github.com/cequals",
+    iconName: "block-open-source/c-equals",
+  },
+  {
+    name: "Spiral",
+    href: "https://spiral.xyz/",
+    iconName: "block-open-source/spiral",
+  },
+];
