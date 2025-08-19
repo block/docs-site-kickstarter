43e60a3364f8fead83372f81795fe03314a15862
Andrew Lee Rubinger <alr@alrubinger.com>
2025-08-19 15:58:05 -0700

    Issue #62:Refactor CSS out of ContentPageLayout
    * Always keep the content centered in the page on very wide viewports when there's no right TableOfContents bar
    * Make sure the breakpoints are all consistent - ie, when losing the left navbar, the mobile navbar option needs to appear
    * Never wrap lines in the header

--- src/components/docs/MobileNavigation.tsx
+++ src/components/docs/MobileNavigation.tsx
@@ -11,7 +11,7 @@ export function MobileNavigation({ children, currentPath }: MobileNavProps) {
   return (
     <SheetPrimitive.Root>
       <SheetPrimitive.Trigger asChild>
-        <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 w-9 md:hidden">
+        <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 w-9 lg:hidden">
           <Menu className="h-5 w-5" />
           <span className="sr-only">Toggle Menu</span>
         </button>
