d066300eb254ac4d4ff836e7dca4d43ce72e04f5
Michael Polce <mpolce@squareup.com>
2025-08-20 14:23:48 -0700

    footer work for issue #60

---
+++ src/components/blockopensource/footer-blockos.ts
@@ -0,0 +1,17 @@
+export interface FooterProps {
+    companyName: string;
+    sections: string[];
+    socialLinks: string[];
+    caption: string;
+}
+
+export const footerBlockOS: FooterProps[] = [
+    {
+        companyName: "Block",
+        sections: [
+            "Company",
+        ],
+        socialLinks: [],
+        caption: "© 2025 Block, Inc. BLOCK and the Block Logo are trademarks of Block, Inc.",
+    },
+];
